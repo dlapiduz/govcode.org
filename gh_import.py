f4211e6cc84f7e375c46b11488c8b35033d5406d
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2013-02-26 08:49:41 -0500

    Change import script to work as a command

--- gh_import.py
+++ gh_import.py
@@ -3,60 +3,74 @@ from pygithub3 import Github
 from models import Organization, Repository, Commit, User
 from extensions import db
 import os
-
-gh = Github(token=os.getenv('GH_TOKEN'))
-
-def import_from_gh():
-    get_orgs()
-    for org in Organization.query.all():
-        get_repos(org)
-        print org
-    for repo in Repository.query.all():
-        get_commits(repo)
-        print repo
-
-    print "Finished Importing"
+from flask.ext.script import Command
 
 
-def get_orgs():
-    url = 'http://registry.usa.gov/accounts.json?service_id=github'
+class GhImport(Command):
 
-    r = requests.get(url)
-    if r.status_code == 200:
-        for acct in r.json['accounts']:
-            org = Organization.query.filter_by(username=acct['account']).first()
-            if org is None:
-                org = Organization()
-                org.name = acct['organization']
-                org.username = acct['account']
-                db.session.add(org)
+    gh = Github(token=os.getenv('GH_TOKEN'))
+
+    def run(self):
+        self.get_orgs()
+        for org in Organization.query.all():
+            self.get_repos(org)
+            print org
+        for repo in Repository.query.all():
+            
+            try:
+                print repo
+                self.get_commits(repo)
+            except Exception, e:
+                db.session.rollback()
+                print 'Error with ' + str(repo) + ": " + str(e)
+                
+
+        print "Finished Importing"
+
+
+    def get_orgs(self):
+        url = 'http://registry.usa.gov/accounts.json?service_id=github'
+
+        r = requests.get(url)
+        if r.status_code == 200:
+            for acct in r.json['accounts']:
+                org = Organization.query.filter_by(username=acct['account']).first()
+                if org is None:
+                    org = Organization()
+                    org.name = acct['organization']
+                    org.username = acct['account']
+                    db.session.add(org)
+            db.session.commit()
+        else:
+            print 'Error importing organizations'
+
+    def get_repos(self, org):
+        repos = self.gh.repos.list_by_org(org.username).all()
+        for repo in repos:
+            if not repo.fork:
+                r = Repository.query.filter_by(gh_id=repo.id).first()
+                if r is None:
+                    r = Repository()
+                r.organization = org
+                r.gh_id = repo.id
+                r.name = repo.name
+                r.description = repo.description
+                r.forks = repo.forks
+                r.watchers = repo.watchers
+                r.size = repo.size
+                r.open_issues = repo.open_issues
+                db.session.add(r)
         db.session.commit()
-    else:
-        print 'Error importing organizations'
 
-def get_repos(org):
-    repos = gh.repos.list_by_org(org.username).all()
-    for repo in repos:
-        if not repo.fork:
-            r = Repository.query.filter_by(gh_id=repo.id).first()
-            if r is None:
-                r = Repository()
-            r.organization = org
-            r.gh_id = repo.id
-            r.name = repo.name
-            r.description = repo.description
-            r.forks = repo.forks
-            r.watchers = repo.watchers
-            r.size = repo.size
-            r.open_issues = repo.open_issues
-            db.session.add(r)
-    db.session.commit()
-
-def get_commits(repo):
-    commit_pages = gh.repos.commits.list(user=repo.organization.username,
-                                        repo=repo.name)
-    for page in commit_pages:
-        for commit in page:
+    def get_commits(self, repo):
+        last_commit = repo.commits.order_by(Commit.date.desc()).first()
+        commit_pages = self.gh.repos.commits.list(user=repo.organization.username,
+                                            repo=repo.name,
+                                            sha="master")
+        for commit in commit_pages.iterator():
+            if last_commit and commit.sha == last_commit.sha:
+                print 'Next'
+                break
             c = Commit.query.filter_by(sha=commit.sha).first()
             if c is None:
                 c = Commit()
@@ -65,17 +79,20 @@ def get_commits(repo):
                 c.repository = repo
                 c.date = commit.commit.author.date
                 if commit.author:
-                    c.author = get_or_create_user(commit.author)
+                    c.user = self.get_or_create_user(commit.author)
+                print 'New commit: ' + c.sha
                 db.session.add(c)
-        db.session.commit()
+                db.session.commit()
 
 
-def get_or_create_user(user):
-    u = User.query.filter_by(gh_id=user.id).first()
-    if u is None:
-        u = User()
-        u.gh_id = user.id
-        u.login = user.login
-        u.avatar_url = user.avatar_url
-        db.session.add(u)
-    return u
+
+    def get_or_create_user(self, user):
+        u = User.query.filter_by(gh_id=user.id).first()
+        if u is None:
+            u = User()
+            u.gh_id = user.id
+            u.login = user.login
+            u.avatar_url = user.avatar_url
+            db.session.add(u)
+            db.session.commit()
+        return u
--- manage.py
+++ manage.py
@@ -10,12 +10,16 @@ app = create_app()
 
 manager = Manager(app)
 
+from gh_import import GhImport
+
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(
     use_debugger=True,
     use_reloader=True,
     host='0.0.0.0')
 )
+
+manager.add_command("do_import", GhImport())
 
 
 if __name__ == "__main__":
