709b235cbde8c9fa357aae99251db78e8726a0b3
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-04-12 12:52:23 -0700

    Change app name if fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,6 @@ from config.deploy import setup
 
 env.hosts = ["govcode.org"]
 env.base_dir = '/www'
-env.app_name = 'govcode.org'
+env.app_name = 'govcode'
 env.git_clone = 'git://github.com/dlapiduz/govcode.git'
 env.restart_cmd = 'kill -HUP `supervisorctl pid gunicorn`'
