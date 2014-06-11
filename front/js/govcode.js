4d26a572454aa64c18da0549e2ed9ec975c6fc24
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-06-11 08:43:09 -0600

    Front end kind of working!

--- front/js/govcode.js
+++ front/js/govcode.js
@@ -2,58 +2,87 @@ var gcControllers = angular.module('gcControllers', []);
 
 gcControllers.controller('MainCtrl', ['$scope', '$location',
   function ($scope, $location) {
-    $scope.getClass = function(path) {
-      if ($location.path().substr(0, path.length) == path) {
-        return "active"
+    $scope.getClass = function (path) {
+      if (($location.path().substr(0, path.length) == path && path != "/") || $location.path() == path) {
+        return "current";
       } else {
-        return ""
+        return "";
       }
     };
 
-    $scope.include = function(actual, expected) {
-        $.each(expected, function(i, el) {
-            if (actual == el) {
-                console.log("True");
-                return true;
-            }
-        });
-        return false;
-    }
   }]
 );
 
 gcControllers.controller('ReposCtrl', ['$scope', '$http',
   function ($scope, $http) {
-    $http.get('http://localhost:3000/repos').success(function(data) {
+    $scope.search = {};
+    $scope.search.orgFilter = {};
+    $http.get('http://localhost:3000/repos').success(function (data) {
       $scope.repos = data;
+      $scope.languages = $.unique($.map(data, function (el, i) { return el.Language }));
+      $scope.search.Language = null;
+
     });
 
     $http.get('http://localhost:3000/orgs').success(function(data) {
       $scope.orgs = data;
-      var ids = $.map(data, function(el, i) { return el.Id });
-      $scope.search = { OrganizationId: ids };
+      $.each(data, function(i, el) { 
+        $scope.search.orgFilter[el.Id] = true;
+      });
     });
+
   }]
 );
 
+
+gcControllers.controller('ContribCtrl', function () {});
+gcControllers.controller('StatsCtrl', function () {});
+gcControllers.controller('AboutCtrl', function () {});
+
 var govcode = angular.module('govcode', ['ngRoute', 'gcControllers', 'truncate', 'ui.unique']);
 
-govcode.filter('includefilter', function() {
+govcode.filter('multifilter', function() {
     return function(items, options) {
-        console.log(items);
-        console.log(options)
-      return items;
+      if (items === undefined) { return };
+      var filtered_items = [];
+      var i;
+      for (i = 0; i < items.length; i++) {
+        // Filter by language
+        if (options.Language !== null && items[i].Language !== options.Language) {
+          continue;
+        }
+        // Filter by org
+        if (!options.orgFilter[items[i].OrganizationId]) {
+          continue;
+        }
+
+        filtered_items.push(items[i]);
+      };
+
+      return filtered_items;
     };
 });
 
 govcode.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/repos', {
+      when('/', {
         templateUrl: 'partials/repos.html',
         controller: 'ReposCtrl'
       }).
+      when('/contributors', {
+        templateUrl: 'partials/contributors.html',
+        controller: 'ContribCtrl'
+      }).
+      when('/stats', {
+        templateUrl: 'partials/stats.html',
+        controller: 'StatsCtrl'
+      }).
+      when('/about', {
+        templateUrl: 'partials/about.html',
+        controller: 'AboutCtrl'
+      }).
       otherwise({
-        redirectTo: '/repos'
+        redirectTo: '/'
       });
 }]);
